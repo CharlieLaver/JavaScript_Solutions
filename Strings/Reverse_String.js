cf113d10c289b129d0a615067a79c38cde3186c6
Charlie Laver <charlielaver7@gmail.com>
2021-01-17 12:46:54 +0000

    Delete Reverse_String.js

--- Strings/Reverse_String.js
+++
@@ -1,8 +0,0 @@
-function solution(str){
-  var splitString = str.split("");
-  var reverseArray = splitString.reverse();
-  var joinArray = reverseArray.join("");
-  return joinArray;
-}
-
-//reverse a string
